f0ab902b19efc3813aaca68be90d71da469e6009
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-16 08:51:12 +0300

    data: wire Gazirat Awlad Hamza follow-up

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -29,6 +29,7 @@ import { liveMapsScan20260916 } from './live-maps-scan-2026-09-16';
 import { awladGabaraEducationScan20260916 } from './awlad-gabara-education-scan-2026-09-16';
 import { nuwairatFollowupScan20260916 } from './nuwairat-followup-scan-2026-09-16';
 import { awladHamzaFollowupScan20260916 } from './awlad-hamza-followup-scan-2026-09-16';
+import { gaziratAwladHamzaFollowup20260916 } from './gazirat-awlad-hamza-followup-2026-09-16';
 
 export * from '../types';
 export { categories, villages };
@@ -62,6 +63,7 @@ const rawListings: DirectoryListing[] = [
   ...awladGabaraEducationScan20260916,
   ...nuwairatFollowupScan20260916,
   ...awladHamzaFollowupScan20260916,
+  ...gaziratAwladHamzaFollowup20260916,
 ];
 
 // Keep immutable IDs/slugs untouched while standardizing user-facing legacy data
