3cb226589fcadac515fe28c194707ca23b8b575b
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-17 09:42:00 +0300

    assets: map Awlad Bahig post office visual

--- lib/directory-images.ts
+++ lib/directory-images.ts
@@ -84,6 +84,7 @@ const curatedListingImages: Record<string, ListingImage> = {
   'worship-مسجد-ال-عسير-التاريخي-اولاد-بهيج': { src: '/images/activities/awlad-bahig-al-asir-mosque.svg' },
   'worship-مسجد-ال-عيسى-اولاد-بهيج': { src: '/images/activities/awlad-bahig-al-eissa-mosque.svg' },
   'shops-تسالي-الشروق-اولاد-بهيج': { src: '/images/activities/awlad-bahig-tasali-al-shorouk.svg' },
+  'government-مكتب-بريد-اولاد-بهيج': { src: '/images/activities/awlad-bahig-post-office.svg' },
   'community-مركز-شباب-الاحايوة-غرب': { src: '/images/activities/ahaywa-gharb-youth-center.svg' },
   'pharmacies-صيدلية-الدكتور-السيد-الاحايوة-غرب': { src: '/images/activities/ahaywa-gharb-al-sayed-pharmacy.svg' },
   'education-معهد-بنين-نجع-عباس-الاعدادي-الثانوي': { src: '/images/activities/nag-abbas-boys-azhar-institute.svg' },
