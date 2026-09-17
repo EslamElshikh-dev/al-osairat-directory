b09fd5f9839a69f2e50a168c35ecd4d2af58d1e7
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-17 03:37:41 +0300

    data: wire Nag Abbas follow-up scan

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -35,6 +35,7 @@ import { shuhadaFollowupScan20260916 } from './shuhada-followup-scan-2026-09-16'
 import { masaeedFollowupScan20260916 } from './masaeed-followup-scan-2026-09-16';
 import { awladBahigFollowupScan20260916 } from './awlad-bahig-followup-scan-2026-09-16';
 import { ahaywaGharbFollowupScan20260917 } from './ahaywa-gharb-followup-scan-2026-09-17';
+import { nagAbbasFollowupScan20260917 } from './nag-abbas-followup-scan-2026-09-17';
 
 export * from '../types';
 export { categories, villages };
@@ -74,6 +75,7 @@ const rawListings: DirectoryListing[] = [
   ...masaeedFollowupScan20260916,
   ...awladBahigFollowupScan20260916,
   ...ahaywaGharbFollowupScan20260917,
+  ...nagAbbasFollowupScan20260917,
 ];
 
 // Keep immutable IDs/slugs untouched while standardizing user-facing legacy data
