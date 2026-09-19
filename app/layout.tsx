bba1a5288a780c9d2c50ff727cce274b7623bdd2
Eslam Elshikh <moqawel1215@gmail.com>
2026-09-19 06:37:36 +0300

    Load Community V2.5 styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ import './community-profiles.css';
 import './community-v2.css';
 import './community-v23.css';
 import './community-v24.css';
+import './community-v25.css';
 import './directory-scroll-fix.css';
 import './directory-ticker.css';
 import './navigation-scroll.css';
